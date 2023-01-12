62074806291a99816e4dd4a0ee78f26b50c6e01c
Juraj Michálek <juraj.michalek@espressif.com>
2023-01-12 12:04:40 +0100

    wip: clean up the code

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,7 @@ use embedded_hal_1::spi::SpiDevice;
 use esp32_hal::{clock::{ClockControl, CpuClock}, pac::Peripherals, prelude::*, spi::{ Spi, SpiBusController, SpiMode}, timer::TimerGroup, Delay, Rtc, IO};
 use esp_backtrace as _;
 
-use epd_waveshare::{prelude::*, color::*, epd2in13_v2::{Display2in13, Epd2in13}};
-
 use esp_println::println;
-
-use embedded_graphics::{
-    mono_font::MonoTextStyleBuilder,
-    prelude::*,
-    text::{Baseline, Text, TextStyleBuilder},
-};
-
 
 
 #[xtensa_lx_rt::entry]
@@ -39,34 +30,12 @@ fn main() -> ! {
 
 
     // Configuration based on https://github.com/Xinyuan-LilyGO/LilyGo-T5-Epaper-Series/blob/master/schematic/T5V2.3.pdf
+    // Flip the board to read values from the back
+    // Some reference values
     // https://github.com/Xinyuan-LilyGO/LilyGo-T5-Epaper-Series/blob/master/lib/GxEPD/src/boards.h
-    //#define EPD_MOSI                (23)
-// #define EPD_MISO                (-1)
-// #define EPD_SCLK                (18)
-// #define EPD_CS                  (5)
+    // C++ implementation
+    // https://github.com/Xinyuan-LilyGO/LilyGo-T5-Epaper-Series/blob/master/lib/GxEPD/src/GxGDEH0213B72/GxGDEH0213B72.cpp
 
-// #define EPD_BUSY                (4)
-// #define EPD_RSET                (16)
-// #define EPD_DC                  (17)
-
-// #define SDCARD_CS               (13)
-// #define SDCARD_MOSI             (15)
-// #define SDCARD_MISO             (2)
-// #define SDCARD_SCLK             (14)
-
-// #define BUTTON_1                (39)
-// #define BUTTONS                 {39}
-
-// #define BUTTON_COUNT            (1)
-
-// #define LED_PIN                 (19)
-// #define LED_ON                  (LOW)
-
-// #define ADC_PIN                 (35)
-
-// #define _HAS_ADC_DETECTED_
-// #define _HAS_LED_
-// #define _HAS_SDCARD_
     delay.delay_ms(10u32);
 
     let busy = io.pins.gpio4.into_floating_input();
@@ -98,9 +67,6 @@ fn main() -> ! {
     while busy.is_high().unwrap() {
         delay.delay_ms(10u32);
     }
-
-
-//https://github.com/Xinyuan-LilyGO/LilyGo-T5-Epaper-Series/blob/master/lib/GxEPD/src/GxGDEH0213B72/GxGDEH0213B72.cpp
 
     println!("Init");
     let write = [0x12];
@@ -144,8 +110,7 @@ fn main() -> ! {
     let mut read: [u8; 4] = [0x00u8; 4];
     eink_device.transfer(&mut read[..], &write[..]).unwrap();
 
-    println!("_SetRamArea");
-    // _SetRamArea
+    println!("Set RAM area");
     let x_pixels_par:u8 = 212 - 1;
     let y_pixels_par:u8 = 104 - 1;
     let x_start:u8 = 0;
@@ -157,79 +122,34 @@ fn main() -> ! {
     let write = [0x44, x_start + 1, x_end + 1, 0x45, y_start, y_start1, y_end, y_end1];
     eink_device.transfer(&mut read[..], &write[..]).unwrap();
 
-    // spi.write_data(&[0x00]).unwrap();
-//     void GxDEPG0213BN::_SetRamArea(uint8_t Xstart, uint8_t Xend, uint8_t Ystart, uint8_t Ystart1, uint8_t Yend, uint8_t Yend1)
-// {
-//     _writeCommand(0x44);
-//     _writeData(Xstart + 1);
-//     _writeData(Xend + 1);
-//     _writeCommand(0x45);
-//     _writeData(Ystart);
-//     _writeData(Ystart1);
-//     _writeData(Yend);
-//     _writeData(Yend1);
-// }
+    println!("Set RAM pointer");
 
-// void GxDEPG0213BN::_SetRamPointer(uint8_t addrX, uint8_t addrY, uint8_t addrY1)
-// {
-//     _writeCommand(0x4e);
-//     _writeData(addrX + 1);
-//     _writeCommand(0x4f);
-//     _writeData(addrY);
-//     _writeData(addrY1);
-// }
-println!("_SetRamPointer");
-
-let write = [0x4e, 0x01, 0x4f, 0x00, 0x00];
+    let write = [0x4e, 0x01, 0x4f, 0x00, 0x00];
     eink_device.transfer(&mut read[..], &write[..]).unwrap();
 
 
-
-    // void GxDEPG0213BN::update(void)
-    // {
-    //     if (_current_page != -1) return;
-    //     _using_partial_mode = false;
-    //     _Init_Full(0x03);
-    //     _writeCommand(0x24);
-    //     for (uint16_t y = 0; y < GxDEPG0213BN_HEIGHT; y++) {
-    //         for (uint16_t x = 0; x < GxDEPG0213BN_WIDTH / 8; x++) {
-    //             uint16_t idx = y * (GxDEPG0213BN_WIDTH / 8) + x;
-    //             uint8_t data = (idx < sizeof(_buffer)) ? _buffer[idx] : 0x00;
-    //             _writeData(~data);
-    //         }
-    //     }
-    //     _Update_Full();
-    //     _PowerOff();
-    // }
-    
-    //  _writeCommand(0x32);
-//   _writeData(LUT_DATA_full, sizeof(LUT_DATA_full));
-//   _PowerOn();
     let write = [0x32];
     eink_device.transfer(&mut read[..], &write[..]).unwrap();
 
     println!("LUT Data");
     let write:[u8;70]=
-[
-  0x80, 0x60, 0x40, 0x00, 0x00, 0x00, 0x00, //LUT0: BB:     VS 0 ~7
-  0x10, 0x60, 0x20, 0x00, 0x00, 0x00, 0x00, //LUT1: BW:     VS 0 ~7
-  0x80, 0x60, 0x40, 0x00, 0x00, 0x00, 0x00, //LUT2: WB:     VS 0 ~7
-  0x10, 0x60, 0x20, 0x00, 0x00, 0x00, 0x00, //LUT3: WW:     VS 0 ~7
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //LUT4: VCOM:   VS 0 ~7
-  0x03, 0x03, 0x00, 0x00, 0x02, // TP0 A~D RP0
-  0x09, 0x09, 0x00, 0x00, 0x02, // TP1 A~D RP1
-  0x03, 0x03, 0x00, 0x00, 0x02, // TP2 A~D RP2
-  0x00, 0x00, 0x00, 0x00, 0x00, // TP3 A~D RP3
-  0x00, 0x00, 0x00, 0x00, 0x00, // TP4 A~D RP4
-  0x00, 0x00, 0x00, 0x00, 0x00, // TP5 A~D RP5
-  0x00, 0x00, 0x00, 0x00, 0x00, // TP6 A~D RP6
-];
+    [
+    0x80, 0x60, 0x40, 0x00, 0x00, 0x00, 0x00, //LUT0: BB:     VS 0 ~7
+    0x10, 0x60, 0x20, 0x00, 0x00, 0x00, 0x00, //LUT1: BW:     VS 0 ~7
+    0x80, 0x60, 0x40, 0x00, 0x00, 0x00, 0x00, //LUT2: WB:     VS 0 ~7
+    0x10, 0x60, 0x20, 0x00, 0x00, 0x00, 0x00, //LUT3: WW:     VS 0 ~7
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //LUT4: VCOM:   VS 0 ~7
+    0x03, 0x03, 0x00, 0x00, 0x02, // TP0 A~D RP0
+    0x09, 0x09, 0x00, 0x00, 0x02, // TP1 A~D RP1
+    0x03, 0x03, 0x00, 0x00, 0x02, // TP2 A~D RP2
+    0x00, 0x00, 0x00, 0x00, 0x00, // TP3 A~D RP3
+    0x00, 0x00, 0x00, 0x00, 0x00, // TP4 A~D RP4
+    0x00, 0x00, 0x00, 0x00, 0x00, // TP5 A~D RP5
+    0x00, 0x00, 0x00, 0x00, 0x00, // TP6 A~D RP6
+    ];
     eink_device.transfer(&mut read[..], &write[..]).unwrap();
 
     println!("Power On");
-    // _writeCommand(0x22);
-    // _writeData(0xc0);
-    // _writeCommand(0x20);
     let write = [0x22, 0xc0, 0x20];
     eink_device.transfer(&mut read[..], &write[..]).unwrap();
     while busy.is_high().unwrap() {
@@ -245,39 +165,15 @@ let write = [0x4e, 0x01, 0x4f, 0x00, 0x00];
     let write = &[0xff;212*104];
     eink_device.transfer(&mut read[..], &write[..]).unwrap();
 
-    // let write = [0x26];
-    // eink_device.transfer(&mut read[..], &write[..]).unwrap();
+    let write = [0x22, 0xc4, 0x20];
+    eink_device.transfer(&mut read[..], &write[..]).unwrap();
 
-
-
-//     void GxDEPG0213BN::_Update_Full(void)
-// {
-//     _writeCommand(0x22);
-//     _writeData(0xc4);
-// //     _writeCommand(0x20);
-//     _waitWhileBusy("_Update_Full", full_refresh_time);
-// }
-let write = [0x22, 0xc4, 0x20];
-eink_device.transfer(&mut read[..], &write[..]).unwrap();
-
-println!("Wait for operation to finish");
-    // spi.write(&[0x20]).unwrap();
+    println!("Wait for operation to finish");
     while busy.is_high().unwrap() {
         delay.delay_ms(100u32);
     }
 
     println!("Power off");
-    // let mut epd = Epd2in13::new(&mut spi, cs.into_push_pull_output(), busy, dc, rst, &mut delay).unwrap();
-    // let mut display = Display2in13::default();
-    // display.set_rotation(DisplayRotation::Rotate90);
-
-    // draw_text(&mut display, " Hello Rust from ESP32! ", 15, 50);
-    // epd.update_and_display_frame(&mut spi, display.buffer(), &mut delay).expect("Frame cannot be cleared and updated!");
-
-//  _writeCommand(0x22);
-// _writeData(0xc3);
-// _writeCommand(0x20);
-// _waitWhileBusy("_PowerOff");
 
     let write = [0x22, 0xc3, 0x20];
     eink_device.transfer(&mut read[..], &write[..]).unwrap();
