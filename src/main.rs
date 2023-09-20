ac697321dff8e859f11e2ddd8f183b456c6162eb
Juraj Michálek <juraj.michalek@espressif.com>
2023-09-20 14:00:14 +0200

    working version of application

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 extern crate alloc;
-use core::mem::MaybeUninit;
+use core::{mem::MaybeUninit, panic};
 use hal::{psram, prelude::*, peripherals::Peripherals, spi, timer::TimerGroup, clock::{ClockControl, CpuClock}, Delay, Rtc, Rng, IO};
 
 use embedded_io::blocking::*;
@@ -40,7 +40,7 @@ const PASSWORD: &str = env!("PASSWORD");
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 
 fn init_heap() {
-    const HEAP_SIZE: usize = 5 * 1024;
+    const HEAP_SIZE: usize = 32 * 1024;
     static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
 
     unsafe {
@@ -113,22 +113,21 @@ fn main() -> ! {
     // Initialize ePaper display
     ssd1680.clear_bw_frame(&mut spi).unwrap();
 
-    // Define the size of the buffer
-    const BUF_SIZE: usize = 4050;
-    use alloc::alloc::{alloc, dealloc, Layout};
-    // Create a layout
-    let layout = Layout::from_size_align(BUF_SIZE, 1).unwrap();
-
     // // Allocate a buffer
     // let buf_ptr = unsafe { alloc(layout) };
     // let buf_array: &mut [u8; 4050] = unsafe {
     //     &mut *(buf_ptr as *mut [u8; 4050])
     // };
-    // let mut nothing_array = alloc::vec![0u8; 25000];
-    // println!("{:?}",nothing_array[0]
-    let mut buf_array = alloc::vec![0u8; 4050];
-    let first_element_ptr = &buf_array[0] as *const u8;
-    println!("Memory address of first element: {:?}", first_element_ptr);
+    let mut rx_buffer = [0u8; 1536];
+    let mut tx_buffer = [0u8; 1536];
+    let mut buffer = [0u8; 512];
+
+    // let mut nothing_array = alloc::vec![0u8; 500];
+    // println!("{:?}",nothing_array[0]);
+    let mut buf_array = alloc::vec![0u8; 4000];
+
+    // let first_element_ptr = &buf_array[0] as *const u8;
+    // println!("Memory address of first element: {:?}", first_element_ptr);
 
     // loop that sets first 1000 bytes of buf_array to 255
     for i in 0..4000 {
@@ -141,40 +140,42 @@ fn main() -> ! {
     // // print size of buf_array
     // println!("buf_array size: {}", buf_array.len());
 
-    println!("buf_array: {:?}", buf_array);
+    // println!("buf_array: {:?}", buf_array);
     println!("Creating display_bw");
-    let mut display_bw = match buf_array.try_into() {
-        Ok(buffer) => {
-            println!("Buffer converted");
-            let display = Display2in13::bw_with_buffer(buffer);
-            println!("Display created");
-            display
-        }
-        ,
-        Err(e) => {
-            // Log the error and handle it as appropriate for your application.
-            println!("Error converting buf_array: {:?}", e);
-            // Returning or handling the error further as needed
-            panic!("Error converting buf_array: {:?}", e);
-        }
-    };
-    // let mut display_bw = Display2in13::bw_with_buffer(buf_array.try_into().unwrap());
+    // let mut display_bw = match buf_array.try_into() {
+    //     Ok(buffer) => {
+    //         println!("Buffer converted");
+    //         let display = Display2in13::bw_with_buffer(buffer);
+    //         println!("Display created");
+    //         display
+    //     }
+    //     ,
+    //     Err(e) => {
+    //         // Log the error and handle it as appropriate for your application.
+    //         println!("Error converting buf_array: {:?}", e);
+    //         // Returning or handling the error further as needed
+    //         panic!("Error converting buf_array: {:?}", e);
+    //     }
+    // };
+    let mut display_bw = Display2in13::bw_with_buffer(buf_array.try_into().unwrap());
     // let mut display_bw = Display2in13::bw();
     println!("Drawing text");
-    draw_text(&mut display_bw, "...", 0, 10); // Assuming draw_text function is defined
 
-    match ssd1680.update_bw_frame(&mut spi, display_bw.buffer()) {
-        Ok(_) => println!("Update frame success"),
-        Err(e) => println!("Update frame error {:?}", e),
-    }
+    // draw_text(&mut display_bw, "...", 0, 10); // Assuming draw_text function is defined
 
-    match ssd1680.display_frame(&mut spi, &mut delay) {
-        Ok(_) => println!("Display frame success"),
-        Err(e) => println!("Display frame error {:?}", e),
-    }
+    // match ssd1680.update_bw_frame(&mut spi, display_bw.buffer()) {
+    //     Ok(_) => println!("Update frame success"),
+    //     Err(e) => println!("Update frame error {:?}", e),
+    // }
+
+    // match ssd1680.display_frame(&mut spi, &mut delay) {
+    //     Ok(_) => println!("Display frame success"),
+    //     Err(e) => println!("Display frame error {:?}", e),
+    // }
 
     println!("Initializing");
     // Initialize WiFi
+    
     let (wifi, ..) = peripherals.RADIO.split();
     println!("Allocating sockets");
     let mut socket_set_entries: [SocketStorage; 5] = Default::default();
@@ -185,15 +186,14 @@ fn main() -> ! {
             Ok(val) => val,
             Err(_) => {
                 let err_msg = "Network init failed";
-                draw_text(&mut display_bw, err_msg, 0, 0); // Assuming draw_text function is defined
-                ssd1680.update_bw_frame(&mut spi, display_bw.buffer()).unwrap();
-                ssd1680.display_frame(&mut spi, &mut delay).unwrap();
+                // draw_text(&mut display_bw, err_msg, 0, 0); // Assuming draw_text function is defined
+                // ssd1680.update_bw_frame(&mut spi, display_bw.buffer()).unwrap();
+                // ssd1680.display_frame(&mut spi, &mut delay).unwrap();
                 // Log the error message to serial console or another debugging interface
                 print!("{}", err_msg);
                 loop {}
             }
     };
-
     println!("Creating WifiStack");
     let wifi_stack = WifiStack::new(iface, device, sockets, current_millis);
     println!("Creating ClientConfiguration");
@@ -205,8 +205,30 @@ fn main() -> ! {
     println!("Setting configuration");
     controller.set_configuration(&client_config).unwrap();
     println!("Starting WiFi controller");
-    controller.start().unwrap();
-    println!("is wifi started: {:?}", controller.is_started());
+    println!("buf_array: {:?}", display_bw.buffer());
+    match controller.start() {
+        Ok(_) => println!("WiFi controller started"),
+        Err(e) => println!("WiFi controller error {:?}", e),
+    }
+    // println!("is wifi started: {:?}", controller.is_started());
+    
+    // println!("Creating display_bw");
+    // let mut display_bw = match buf_array.try_into() {
+    //     Ok(buffer) => {
+    //         println!("Buffer converted");
+    //         let display = Display2in13::bw_with_buffer(buffer);
+    //         println!("Display created");
+    //         display
+    //     }
+    //     ,
+    //     Err(e) => {
+    //         // Log the error and handle it as appropriate for your application.
+    //         println!("Error converting buf_array: {:?}", e);
+    //         // Returning or handling the error further as needed
+    //         panic!("Error converting buf_array: {:?}", e);
+    //     }
+    // };
+    // let mut display_bw = Display2in13::bw_with_buffer(buf_array.try_into().unwrap());
 
     println!("Start Wifi Scan");
     let res: Result<(heapless::Vec<AccessPointInfo, 10>, usize), WifiError> = controller.scan_n();
@@ -249,11 +271,10 @@ fn main() -> ! {
             break;
         }
     }
-
+    
     println!("Start busy loop on main");
 
-    let mut rx_buffer = [0u8; 1536];
-    let mut tx_buffer = [0u8; 1536];
+
     let mut socket = wifi_stack.get_socket(&mut rx_buffer, &mut tx_buffer);
 
     loop {
@@ -284,9 +305,10 @@ fn main() -> ! {
         let mut full_len = 0;
 
         println!("Reading response");
+        
         // Read the HTTP response into the buffer.
         loop {
-            let mut buffer = [0u8; 512];
+            
             if let Ok(len) = socket.read(&mut buffer) {
                 // Copy the newly read bytes into `full_response`.
                 full_response[full_len..full_len + len].copy_from_slice(&buffer[0..len]);
@@ -311,7 +333,7 @@ fn main() -> ! {
                 break;
             }
         }
-
+        
         if body_start != 0 {
             let body = &full_response[body_start..full_len];
             let to_print = unsafe { core::str::from_utf8_unchecked(body) };
@@ -328,19 +350,9 @@ fn main() -> ! {
         println!("Updating display");
         ssd1680.display_frame(&mut spi, &mut delay).unwrap();
 
-        println!("Updating frame2");
-        ssd1680.update_bw_frame(&mut spi, display_bw.buffer()).unwrap();
-        println!("Updating display2");
-        ssd1680.display_frame(&mut spi, &mut delay).unwrap();
-
-        println!("Updating frame3");
-        ssd1680.update_bw_frame(&mut spi, display_bw.buffer()).unwrap();
-        println!("Updating display3");
-        ssd1680.display_frame(&mut spi, &mut delay).unwrap();
-
         println!("Sleeping");
         // Delay before repeating
-        delay.delay_ms(500000u32);
+        delay.delay_ms(10000u32);
         println!("Waking up");
     }
 }
