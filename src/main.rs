24af822453aeffeac5f68ee92c52dff2da71659f
Juraj Michálek <juraj.michalek@espressif.com>
2023-09-20 11:49:06 +0200

    version which has bug with WiFi

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@
 
 extern crate alloc;
 use core::mem::MaybeUninit;
-
 use hal::{psram, prelude::*, peripherals::Peripherals, spi, timer::TimerGroup, clock::{ClockControl, CpuClock}, Delay, Rtc, Rng, IO};
 
 use embedded_io::blocking::*;
@@ -41,7 +40,7 @@ const PASSWORD: &str = env!("PASSWORD");
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 
 fn init_heap() {
-    const HEAP_SIZE: usize = 32 * 1024;
+    const HEAP_SIZE: usize = 5 * 1024;
     static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
 
     unsafe {
@@ -57,6 +56,11 @@ fn draw_text(display: &mut Display2in13, text: &str, x: i32, y: i32) {
             background_color = White
         ))
         .draw(display);
+}
+
+
+const fn buffer_len(width: usize, height: usize) -> usize {
+    (width + 7) / 8 * height
 }
 
 #[entry]
@@ -115,18 +119,59 @@ fn main() -> ! {
     // Create a layout
     let layout = Layout::from_size_align(BUF_SIZE, 1).unwrap();
 
-    // Allocate a buffer
-    let buf_ptr = unsafe { alloc(layout) };
-    let buf_array: &mut [u8; 4050] = unsafe {
-        &mut *(buf_ptr as *mut [u8; 4050])
-    };
-    // let buf_array = alloc::vec![0u8; 4000];
-    let mut display_bw = Display2in13::bw_with_buffer(*buf_array);
+    // // Allocate a buffer
+    // let buf_ptr = unsafe { alloc(layout) };
+    // let buf_array: &mut [u8; 4050] = unsafe {
+    //     &mut *(buf_ptr as *mut [u8; 4050])
+    // };
+    // let mut nothing_array = alloc::vec![0u8; 25000];
+    // println!("{:?}",nothing_array[0]
+    let mut buf_array = alloc::vec![0u8; 4050];
+    let first_element_ptr = &buf_array[0] as *const u8;
+    println!("Memory address of first element: {:?}", first_element_ptr);
 
+    // loop that sets first 1000 bytes of buf_array to 255
+    for i in 0..4000 {
+        buf_array[i] = 255;
+    }
+
+
+    // print buf size for width 122 and height 250
+    // println!("buf size: {}", buffer_len(122, 250));
+    // // print size of buf_array
+    // println!("buf_array size: {}", buf_array.len());
+
+    println!("buf_array: {:?}", buf_array);
+    println!("Creating display_bw");
+    let mut display_bw = match buf_array.try_into() {
+        Ok(buffer) => {
+            println!("Buffer converted");
+            let display = Display2in13::bw_with_buffer(buffer);
+            println!("Display created");
+            display
+        }
+        ,
+        Err(e) => {
+            // Log the error and handle it as appropriate for your application.
+            println!("Error converting buf_array: {:?}", e);
+            // Returning or handling the error further as needed
+            panic!("Error converting buf_array: {:?}", e);
+        }
+    };
+    // let mut display_bw = Display2in13::bw_with_buffer(buf_array.try_into().unwrap());
+    // let mut display_bw = Display2in13::bw();
+    println!("Drawing text");
     draw_text(&mut display_bw, "...", 0, 10); // Assuming draw_text function is defined
 
-    ssd1680.update_bw_frame(&mut spi, display_bw.buffer()).unwrap();
-    ssd1680.display_frame(&mut spi, &mut delay).unwrap();
+    match ssd1680.update_bw_frame(&mut spi, display_bw.buffer()) {
+        Ok(_) => println!("Update frame success"),
+        Err(e) => println!("Update frame error {:?}", e),
+    }
+
+    match ssd1680.display_frame(&mut spi, &mut delay) {
+        Ok(_) => println!("Display frame success"),
+        Err(e) => println!("Display frame error {:?}", e),
+    }
 
     println!("Initializing");
     // Initialize WiFi
@@ -281,6 +326,16 @@ fn main() -> ! {
         println!("Updating frame");
         ssd1680.update_bw_frame(&mut spi, display_bw.buffer()).unwrap();
         println!("Updating display");
+        ssd1680.display_frame(&mut spi, &mut delay).unwrap();
+
+        println!("Updating frame2");
+        ssd1680.update_bw_frame(&mut spi, display_bw.buffer()).unwrap();
+        println!("Updating display2");
+        ssd1680.display_frame(&mut spi, &mut delay).unwrap();
+
+        println!("Updating frame3");
+        ssd1680.update_bw_frame(&mut spi, display_bw.buffer()).unwrap();
+        println!("Updating display3");
         ssd1680.display_frame(&mut spi, &mut delay).unwrap();
 
         println!("Sleeping");
